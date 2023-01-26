f8aca77abc9fd889a829e1bad62c2aa15f2a7a1e
AnTu111 <andrei.tupitsin@gmail.com>
2023-01-27 00:00:10 +0200

    Funktsioonid

--- PythonApplication1/Funktsioonid.py
+++ PythonApplication1/Funktsioonid.py
@@ -16,3 +16,46 @@ def arithmetic(a1:float,sym:str,a2:float)->any:
     return vas
 
 
+def is_year_leap(a1:str)->str:
+    """Liigaasta kalkulaator
+    Args:
+        a1 (int): year
+    Returns:
+        str:True/Fals
+    """
+    return a1 % 4 ==0
+
+
+
+def season(x:int)->str:
+    """Aasta periood
+        Args:
+            x (int): numbers (1-12)
+        Returns:
+            str:[talv, kevad, suvi, sügis]
+        """
+    if x in range(1,13):
+        if x in range(3,6):
+            vastus = "kevad"
+        elif x in range(6,9):
+            vastus = "suvi"
+        elif x in range(9,12):
+            vastus = "sugis"
+        else:
+            vastus = "talv"
+    else:
+        "Vele number!"
+    return vastus
+
+
+
+def bank(money:float,years:int)->float:
+    """Panga prosent
+    Args:
+        money (float): alg summa
+        yeras(int): aastad
+    Returns:
+        lõppsumma (str): lõpsumma
+    """
+    summa = money*(1+0.1*years)
+    return summa
---
+++ PythonApplication1/Homework_functions.py
@@ -0,0 +1,19 @@
+from Funktsioonid import*
+a = arithmetic(9,"-",1)
+print(a)
+
+
+
+b = is_year_leap(2007)
+print(b)
+
+
+c = season(5)
+print(c)
+
+
+d = bank(100,2)
+print(d)
+
+
+
